458f9261a30dfd8bfd5bf5414515e5087cc62ada
isdance <isdance2004@hotmail.com>
2020-06-10 07:51:15 +1000

    test ci

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,5 +6,5 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'bbxit.au.auth0.com', // Auth0 domain
   clientId: 'tpR1o6tgQ36fokctTUjAiOqkZjuanScu', // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://d3nnwi4yblxpkc.cloudfront.net/callback'
 }
