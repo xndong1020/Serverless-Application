72f8934d36473d1817e1e302ce7815ee0bbebb6c
isdance <isdance2004@hotmail.com>
2020-06-10 13:22:11 +1000

    update callbackUrl to cloudFront url

--- client/src/config.ts
+++ client/src/config.ts
@@ -16,6 +16,7 @@ export const authConfig =
         // TODO: Create an Auth0 application and copy values from it into this map
         domain: 'bbxit.au.auth0.com', // Auth0 domain
         clientId: 'tpR1o6tgQ36fokctTUjAiOqkZjuanScu', // Auth0 client id
-        callbackUrl:
-          'http://my-cloud-dev-s3.s3-website-us-west-2.amazonaws.com/callback'
+        // callbackUrl:
+        //   'http://my-cloud-dev-s3.s3-website-us-west-2.amazonaws.com/callback'
+        callbackUrl: 'd2meud1qv9x340.cloudfront.net'
       }
