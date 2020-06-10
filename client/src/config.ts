f2d3b406a9c66224a4d90a9759a9480691f91a86
isdance <isdance2004@hotmail.com>
2020-06-10 15:30:45 +1000

    test

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,6 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com
 
 const mode = process.env.NODE_ENV
 
-// callbackUrl: 'http://my-cloud-dev-s3.s3-website-us-west-2.amazonaws.com/callback'
 export const authConfig =
   mode === 'development'
     ? {
@@ -17,5 +16,6 @@ export const authConfig =
         // TODO: Create an Auth0 application and copy values from it into this map
         domain: 'bbxit.au.auth0.com', // Auth0 domain
         clientId: 'tpR1o6tgQ36fokctTUjAiOqkZjuanScu', // Auth0 client id
-        callbackUrl: 'http://d2meud1qv9x340.cloudfront.net'
+        callbackUrl:
+          'http://my-cloud-dev-s3.s3-website-us-west-2.amazonaws.com/callback'
       }
