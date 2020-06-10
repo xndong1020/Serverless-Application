1d43a518bb25d1dbbbae257fd817ad3213f62259
isdance <isdance2004@hotmail.com>
2020-06-10 11:50:46 +1000

    config file for build

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,9 +2,20 @@
 const apiId = '2dn9xrtl96'
 export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com/dev`
 
-export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: 'bbxit.au.auth0.com', // Auth0 domain
-  clientId: 'tpR1o6tgQ36fokctTUjAiOqkZjuanScu', // Auth0 client id
-  callbackUrl: 'http://my-cloud-dev-s3.s3-website-us-west-2.amazonaws.com/callback'
-}
+const mode = process.env.NODE_ENV
+
+export const authConfig =
+  mode === 'development'
+    ? {
+        // TODO: Create an Auth0 application and copy values from it into this map
+        domain: 'bbxit.au.auth0.com', // Auth0 domain
+        clientId: 'tpR1o6tgQ36fokctTUjAiOqkZjuanScu', // Auth0 client id
+        callbackUrl: 'http://localhost:3000/callback'
+      }
+    : {
+        // TODO: Create an Auth0 application and copy values from it into this map
+        domain: 'bbxit.au.auth0.com', // Auth0 domain
+        clientId: 'tpR1o6tgQ36fokctTUjAiOqkZjuanScu', // Auth0 client id
+        callbackUrl:
+          'http://my-cloud-dev-s3.s3-website-us-west-2.amazonaws.com/callback'
+      }
